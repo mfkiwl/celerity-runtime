26fa97b9e5b7103b086aad253a6766621e0888de
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-01-12 12:25:01 +0100

    [rm] queue capture API

--- include/distr_queue.h
+++ include/distr_queue.h
@@ -3,25 +3,123 @@
 #include <memory>
 #include <type_traits>
 
+#include "host_object.h"
 #include "runtime.h"
 #include "task_manager.h"
 
 namespace celerity {
 namespace detail {
 
-	class distr_queue_tracker {
-	  public:
-		~distr_queue_tracker() { runtime::get_instance().shutdown(); }
-	};
-
 	template <typename CGF>
 	constexpr bool is_safe_cgf = std::is_standard_layout<CGF>::value;
 
+	struct capture_exfiltrator {
+	  public:
+		template <typename Capture>
+		static auto exfiltrate_by_copy(const Capture& cap) {
+			return cap.exfiltrate_by_copy();
+		}
+
+		template <typename Capture>
+		static auto exfiltrate_by_move(const Capture& cap) {
+			return cap.exfiltrate_by_move();
+		}
+
+		template <typename Captures>
+		static auto exfiltrate_tuple_by_copy(const Captures& caps) {
+			return exfiltrate_tuple_by_copy(caps, std::make_index_sequence<std::tuple_size_v<Captures>>{});
+		}
+
+		template <typename Captures>
+		static auto exfiltrate_tuple_by_move(const Captures& caps) {
+			return exfiltrate_tuple_by_move(caps, std::make_index_sequence<std::tuple_size_v<Captures>>{});
+		}
+
+	  private:
+		template <typename Captures, size_t... Is>
+		static auto exfiltrate_tuple_by_copy(const Captures& caps, std::index_sequence<Is...>) {
+			return std::tuple{std::get<Is>(caps).exfiltrate_by_copy()...};
+		}
+
+		template <typename Captures, size_t... Is>
+		static auto exfiltrate_tuple_by_move(const Captures& caps, std::index_sequence<Is...>) {
+			return std::tuple{std::get<Is>(caps).exfiltrate_by_move()...};
+		}
+	};
+
 } // namespace detail
+
+class distr_queue;
 
 struct allow_by_ref_t {};
 
 inline constexpr allow_by_ref_t allow_by_ref{};
+
+namespace experimental {
+
+	template <typename>
+	class capture;
+
+	template <typename T, int Dims>
+	class buffer_data {
+	  public:
+		buffer_data() : data{0, detail::zero_range} {}
+
+		explicit operator bool() const { return data.get_pointer() != nullptr; }
+
+		celerity::range<Dims> get_range() const { return detail::range_cast<Dims>(data.get_range()); }
+		const T* get_pointer() const { return static_cast<T>(data.get_pointer()); }
+
+	  private:
+		friend class detail::capture_exfiltrator;
+
+		detail::raw_buffer_data data;
+
+		explicit buffer_data(detail::raw_buffer_data raw) : data{std::move(raw)} { assert(data.get_size() / data.get_range().size() == sizeof(T)); }
+	};
+
+	template <typename T, int Dims>
+	class capture<buffer<T, Dims>> {
+	  public:
+		using value_type = buffer_data<T, Dims>;
+
+		explicit capture(const buffer<T, Dims>& buf) : bid{detail::get_buffer_id(buf)}, sr{{{}, buf.get_range()}} {}
+		explicit capture(const buffer<T, Dims>& buf, const subrange<Dims>& sr) : bid{detail::get_buffer_id(buf)}, sr{sr} {}
+
+	  private:
+		friend class detail::capture_exfiltrator;
+
+		detail::buffer_id bid;
+		subrange<Dims> sr;
+
+		value_type exfiltrate_by_copy() const {
+			auto& bm = detail::runtime::get_instance().get_buffer_manager();
+			return value_type{bm.get_buffer_data(bid, sr.offset, sr.range)};
+		}
+
+		value_type exfiltrate_by_move() const { return exfiltrate_by_copy(); }
+	};
+
+	template <typename T>
+	class capture<host_object<T>> {
+	  public:
+		static_assert(std::is_object_v<T>);
+
+		using value_type = T;
+
+		explicit capture(host_object<T> ho) : ho{std::move(ho)} {}
+
+	  private:
+		friend class celerity::distr_queue;
+
+		host_object<T> ho;
+
+		value_type exfiltrate_by_copy() const { return value_type{std::as_const(*ho.get_object())}; }
+
+		value_type exfiltrate_by_move() const { return value_type{std::move(*ho.get_object())}; }
+	};
+
+} // namespace experimental
 
 class distr_queue {
   public:
@@ -31,11 +129,10 @@ class distr_queue {
 		init(&device);
 	}
 
-	distr_queue(const distr_queue&) = default;
-	distr_queue(distr_queue&&) = default;
-
+	distr_queue(const distr_queue&) = delete;
 	distr_queue& operator=(const distr_queue&) = delete;
-	distr_queue& operator=(distr_queue&&) = delete;
+
+	~distr_queue() { detail::runtime::get_instance().shutdown(); }
 
 	/**
 	 * Submits a command group to the queue.
@@ -74,9 +171,21 @@ class distr_queue {
 	 */
 	void slow_full_sync() { detail::runtime::get_instance().sync(); }
 
-  private:
-	std::shared_ptr<detail::distr_queue_tracker> tracker;
+	template <typename T>
+	typename experimental::capture<T>::value_type slow_full_sync(const experimental::capture<T>& cap) {
+		// TODO schedule transfers
+		slow_full_sync();
+		return detail::capture_exfiltrator::exfiltrate_by_copy(cap);
+	}
 
+	template <typename... Ts>
+	std::tuple<typename experimental::capture<Ts>::value_type...> slow_full_sync(const std::tuple<experimental::capture<Ts>...>& caps) {
+		// TODO schedule transfers
+		slow_full_sync();
+		return detail::capture_exfiltrator::exfiltrate_tuple_by_copy(caps);
+	}
+
+  private:
 	void init(cl::sycl::device* user_device) {
 		if(!detail::runtime::is_initialized()) { detail::runtime::init(nullptr, nullptr, user_device); }
 		try {
@@ -84,8 +193,26 @@ class distr_queue {
 		} catch(detail::runtime_already_started_error&) {
 			throw std::runtime_error("Only one celerity::distr_queue can be created per process (but it can be copied!)");
 		}
-		tracker = std::make_shared<detail::distr_queue_tracker>();
 	}
 };
 
+namespace experimental {
+
+	void drain(distr_queue&&) { detail::runtime::get_instance().shutdown(); }
+
+	template <typename T>
+	typename experimental::capture<T>::value_type drain(distr_queue&& q, const experimental::capture<T>& cap) {
+		// TODO schedule transfers
+		drain(std::move(q));
+		return detail::capture_exfiltrator::exfiltrate_by_move(cap);
+	}
+
+	template <typename... Ts>
+	std::tuple<typename experimental::capture<Ts>::value_type...> drain(distr_queue&& q, const std::tuple<experimental::capture<Ts>...>& caps) {
+		// TODO schedule transfers
+		drain(std::move(q));
+		return detail::capture_exfiltrator::exfiltrate_tuple_by_move(caps);
+	}
+
+} // namespace experimental
 } // namespace celerity
