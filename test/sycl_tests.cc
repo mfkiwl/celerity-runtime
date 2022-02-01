79ea00e0bca7e446a40e0ac879a34139a61be23e
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-02-01 18:08:58 +0100

    SYCL test: capture a placeholder accessor without require()ing it

--- test/sycl_tests.cc
+++ test/sycl_tests.cc
@@ -62,4 +62,40 @@ TEMPLATE_TEST_CASE_METHOD_SIG(dim_device_queue_fixture, "ranged_sycl_access work
 	CHECK(actual == expected);
 }
 
+template <access_mode Mode>
+class access_mode_device_queue_fixture : public test_utils::device_queue_fixture {
+  public:
+#if WORKAROUND_DPCPP || WORKAROUND(COMPUTECPP, 2, 7)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations" // target::gobal_buffer is now target::device, but only for very recent versions of DPC++
+	using accessor = sycl::accessor<float, 1, Mode, sycl::access::target::global_buffer, cl::sycl::access::placeholder::true_t>;
+#pragma GCC diagnostic pop
+#elif WORKAROUND_COMPUTECPP
+	using accessor = sycl::accessor<float, 1, Mode, sycl::access::target::device, cl::sycl::access::placeholder::true_t>;
+#else
+	using accessor = sycl::accessor<float, 1, Mode, sycl::target::device>;
+#endif
+
+#if WORKAROUND_DPCPP
+	accessor make_accessor() {
+		sycl::buffer<float, 1> faux_buf{{ 1 }};
+		return accessor{faux_buf};
+	}
+#else
+	accessor make_accessor() { return accessor{}; }
+#endif
+};
+
+template <access_mode>
+class placeholder_accessor_test_kernel;
+
+TEMPLATE_TEST_CASE_METHOD_SIG(access_mode_device_queue_fixture, "SYCL allows capturing a placeholder accessor without cgh.require()ing it", "[]",
+    ((access_mode Mode), Mode), access_mode::read, access_mode::read_write, access_mode::discard_write) {
+	using fixture = access_mode_device_queue_fixture<Mode>;
+	const auto placeholder = fixture::make_accessor();
+	fixture::get_device_queue().submit([&](sycl::handler& cgh) {
+		cgh.parallel_for<placeholder_accessor_test_kernel<Mode>>(sycl::range<1>{10}, [placeholder](sycl::item<1>) { (void)placeholder; });
+	});
+}
+
 } // namespace celerity::detail
